d4d2b1bb00f973a0b23de528c2950ccc1b628c3c
Katerina <comina.ru@gmail.com>
2016-10-11 16:32:19 +0300

    Правки

--- src/js/check.js
+++ src/js/check.js
@@ -1,17 +1,14 @@
 function getMessage(a, b) {
-	var a;
-	var b;
-	if (typeof a === "boolean") {
+	if (typeof a === 'boolean') {
 		if (a) {
-			return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
-		}
-		else {
-			return "Переданное GIF-изображение не анимировано";
+			return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
+		} else {
+			return 'Переданное GIF-изображение не анимировано';
 		}
 	} 
 	
-	if (typeof a === "number") {
-		return "Переданное SVG-изображение содержит " + a + " объектов и " + (b * 4) + " атрибутов";
+	if (typeof a === 'number') {
+		return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' атрибутов';
 	} 
 	
 	if (Array.isArray(a)) {
@@ -20,19 +17,17 @@ function getMessage(a, b) {
 			for (var i = 0; i < a.length; i++) {
         		artifactsSquare += a[i] * b[i];
     		}
-			return "Общая площадь артефактов сжатия: " + artifactsSquare + " пикселей";
+			return 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
 		} else {
 			var amountOfRedPoints = 0;
 			for (var i = 0; i < a.length; i++) {
 				amountOfRedPoints += a[i];
 			}
-			return "Количество красных точек во всех строчках изображения: " + amountOfRedPoints;
+			return 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
 		}
 		
 	} 
 	
-	else {
-		return "Переданы некорректные данные";
-	}
+    return 'Переданы некорректные данные';
 	
 }
