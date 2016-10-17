61b141ea166fecdb4b52f744c63ce8873f3db097
Komnatnaya <comina.ru@gmail.com>
2016-10-17 20:08:15 +0300

    таб в два пробела

--- src/js/check.js
+++ src/js/check.js
@@ -6,17 +6,17 @@ function getMessage(a, b) {
 			return 'Переданное GIF-изображение не анимировано';
 		}
 	}
-	
+
 	if (typeof a === 'number') {
 		return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' атрибутов';
-	} 
-	
+	}
+
 	if (Array.isArray(a)) {
 		if (Array.isArray(b)){
 			var artifactsSquare = 0;
 			for (var i = 0; i < a.length; i++) {
 				artifactsSquare += a[i] * b[i];
-    		}
+		}
 			return 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
 		} else {
 			var amountOfRedPoints = 0;
@@ -25,9 +25,9 @@ function getMessage(a, b) {
 			}
 			return 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
 		}
-		
+
 	}
-	
+
 	return 'Переданы некорректные данные';
-	
+
 }
