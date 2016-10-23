0a4f6bd0796d42130d6588d6707260108f944989
Komnatnaya <comina.ru@gmail.com>
2016-10-23 16:49:31 +0300

    правка

--- src/js/resizer.js
+++ src/js/resizer.js
@@ -137,20 +137,16 @@
 //      this._ctx.rect(-containerWidthHalf, -containerHeightHalf, containerWidthHalf - resizeConstraintSideHalf - this._ctx.lineWidth, this._container.height);
 //      this._ctx.rect(containerWidthHalf, containerHeightHalf, -this._container.width, -containerHeightHalf + resizeConstraintSideHalf - lineWidthHalf);
 //      this._ctx.rect(containerWidthHalf, containerHeightHalf, -containerWidthHalf + resizeConstraintSideHalf - lineWidthHalf, -this._container.height);
+//      this._ctx.fill();
 
 //      this._ctx.rect(-this._container.width / 2, -this._container.height / 2, this._container.width, this._container.height / 2 - this._resizeConstraint.side / 2 - this._ctx.lineWidth);
 //      this._ctx.rect(-this._container.width / 2, -this._container.height / 2, this._container.width / 2 - this._resizeConstraint.side / 2 - this._ctx.lineWidth, this._container.height);
 //      this._ctx.rect(this._container.width / 2, this._container.height / 2, -this._container.width, -this._container.height / 2 + this._resizeConstraint.side / 2 - this._ctx.lineWidth / 2);
 //      this._ctx.rect(this._container.width / 2, this._container.height / 2, -this._container.width / 2 + this._resizeConstraint.side / 2 - this._ctx.lineWidth / 2, -this._container.height);
-			this._ctx.fill();
+//			this._ctx.fill();
 
-      //Размеры изображения
-      this._ctx.font = '20px sans-serif';
-      this._ctx.textAlign = 'center';
-      this._ctx.fillStyle = '#ffffff';
-      this._ctx.fillText(this._image.naturalWidth + ' X ' + this._image.naturalHeight, 0, -resizeConstraintSideHalf - this._ctx.lineWidth * 2);
 
-      Проверка альтернативного способа рисовки темной области 80% прозрачности
+//      Проверка альтернативного способа рисовки темной области 80% прозрачности
       this._ctx.fillStyle = 'rgba(0,0,0,0.8)';
       this._ctx.beginPath();
       this._ctx.moveTo(-containerWidthHalf, -containerHeightHalf);
@@ -165,6 +161,11 @@
       this._ctx.lineTo(-resizeConstraintSideHalf - this._ctx.lineWidth, -resizeConstraintSideHalf -this._ctx.lineWidth);
       this._ctx.fill('evenodd');
 
+      //Размеры изображения
+      this._ctx.font = '20px sans-serif';
+      this._ctx.textAlign = 'center';
+      this._ctx.fillStyle = '#ffffff';
+      this._ctx.fillText(this._image.naturalWidth + ' X ' + this._image.naturalHeight, 0, -resizeConstraintSideHalf - this._ctx.lineWidth * 2);
 
       // Восстановление состояния канваса, которое было до вызова ctx.save
       // и последующего изменения системы координат. Нужно для того, чтобы
